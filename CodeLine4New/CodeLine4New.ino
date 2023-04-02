d565d98007fc8acc04db6800c4be6125338da142
NaCaNe <minuteloveduck@gmail.com>
2023-04-02 08:52:28 +0700

    Daiwibi

---
+++ CodeLine4New/CodeLine4New.ino
@@ -0,0 +1,332 @@
+//⢸⣿⣿⣿⣿⠃⠄⢀⣴⡾⠃⠄⠄⠄⠄⠄⠈⠺⠟⠛⠛⠛⠛⠻⢿⣿⣿⣿⣿⣶⣤⡀⠄
+//⢸⣿⣿⣿⡟⢀⣴⣿⡿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣸⣿⣿⣿⣿⣿⣿⣿⣷
+//⢸⣿⣿⠟⣴⣿⡿⡟⡼⢹⣷⢲⡶⣖⣾⣶⢄⠄⠄⠄⠄⠄⢀⣼⣿⢿⣿⣿⣿⣿⣿⣿⣿
+//⢸⣿⢫⣾⣿⡟⣾⡸⢠⡿⢳⡿⠍⣼⣿⢏⣿⣷⢄⡀⠄⢠⣾⢻⣿⣸⣿⣿⣿⣿⣿⣿⣿
+//⡿⣡⣿⣿⡟⡼⡁⠁⣰⠂⡾⠉⢨⣿⠃⣿⡿⠍⣾⣟⢤⣿⢇⣿⢇⣿⣿⢿⣿⣿⣿⣿⣿
+//⣱⣿⣿⡟⡐⣰⣧⡷⣿⣴⣧⣤⣼⣯⢸⡿⠁⣰⠟⢀⣼⠏⣲⠏⢸⣿⡟⣿⣿⣿⣿⣿⣿
+//⣿⣿⡟⠁⠄⠟⣁⠄⢡⣿⣿⣿⣿⣿⣿⣦⣼⢟⢀⡼⠃⡹⠃⡀⢸⡿⢸⣿⣿⣿⣿⣿⡟
+//⣿⣿⠃⠄⢀⣾⠋⠓⢰⣿⣿⣿⣿⣿⣿⠿⣿⣿⣾⣅⢔⣕⡇⡇⡼⢁⣿⣿⣿⣿⣿⣿⢣
+//⣿⡟⠄⠄⣾⣇⠷⣢⣿⣿⣿⣿⣿⣿⣿⣭⣀⡈⠙⢿⣿⣿⡇⡧⢁⣾⣿⣿⣿⣿⣿⢏⣾
+//⣿⡇⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢻⠇⠄⠄⢿⣿⡇⢡⣾⣿⣿⣿⣿⣿⣏⣼⣿
+//⣿⣷⢰⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⢰⣧⣀⡄⢀⠘⡿⣰⣿⣿⣿⣿⣿⣿⠟⣼⣿⣿
+//⢹⣿⢸⣿⣿⠟⠻⢿⣿⣿⣿⣿⣿⣿⣿⣶⣭⣉⣤⣿⢈⣼⣿⣿⣿⣿⣿⣿⠏⣾⣹⣿⣿
+//⢸⠇⡜⣿⡟⠄⠄⠄⠈⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟⣱⣻⣿⣿⣿⣿⣿⠟⠁⢳⠃⣿⣿⣿
+//⠄⣰⡗⠹⣿⣄⠄⠄⠄⢀⣿⣿⣿⣿⣿⣿⠟⣅⣥⣿⣿⣿⣿⠿⠋⠄⠄⣾⡌⢠⣿⡿⠃
+//⠜⠋⢠⣷⢻⣿⣿⣶⣾⣿⣿⣿⣿⠿⣛⣥⣾⣿⠿⠟⠛⠉⠄⠄
+//
+//                                                                                                ,---,    ,---,    ,---,  
+//         ,--.                                                                  ,--.          ,`--.' | ,`--.' | ,`--.' |  
+//       ,--.'|   ,---,                 ,----..     ,---,                      ,--.'|    ,---,.|   :  : |   :  : |   :  :  
+//   ,--,:  : |  '  .' \               /   /   \   '  .' \                 ,--,:  : |  ,'  .' |'   '  ; '   '  ; '   '  ;  
+//,`--.'`|  ' : /  ;    '.            |   :     : /  ;    '.            ,`--.'`|  ' :,---.'   ||   |  | |   |  | |   |  |  
+//|   :  :  | |:  :       \           .   |  ;. /:  :       \           |   :  :  | ||   |   .''   :  ; '   :  ; '   :  ;  
+//:   |   \ | ::  |   /\   \          .   ; /--` :  |   /\   \          :   |   \ | ::   :  |-,|   |  ' |   |  ' |   |  '  
+//|   : '  '; ||  :  ' ;.   :         ;   | ;    |  :  ' ;.   :         |   : '  '; |:   |  ;/|'   :  | '   :  | '   :  |  
+//'   ' ;.    ;|  |  ;/  \   \        |   : |    |  |  ;/  \   \        '   ' ;.    ;|   :   .';   |  ; ;   |  ; ;   |  ;  
+//|   | | \   |'  :  | \  \ ,'        .   | '___ '  :  | \  \ ,'        |   | | \   ||   |  |-,`---'. | `---'. | `---'. |  
+//'   : |  ; .'|  |  '  '--'          '   ; : .'||  |  '  '--'          '   : |  ; .''   :  ;/| `--..`;  `--..`;  `--..`;  
+//|   | '`--'  |  :  :                '   | '/  :|  :  :                |   | '`--'  |   |    \.--,_    .--,_    .--,_     
+//'   : |      |  | ,'                |   :    / |  | ,'                '   : |      |   :   .'|    |`. |    |`. |    |`.  
+//;   |.'      `--''                   \   \ .'  `--''                  ;   |.'      |   | ,'  `-- -`, ;`-- -`, ;`-- -`, ; 
+//'---'                                 `---`                           '---'        `----'      '---`"   '---`"   '---`"  
+                                                                                                                         
+
+#include <QTRSensors.h>
+#include <NewPing.h>
+
+//materials
+#define btn1 0
+#define led1 13
+
+//motors
+#define bin1 8
+#define bin2 7
+#define pwmb 6
+#define ain1 3
+#define ain2 4
+#define pwma 5
+
+//HC_SR04
+#define trig 11 //chân trig của HC-SR04
+#define echo 12//chân echo của HC-SR04
+#define maximum_distance 200
+
+//QTR
+#define NUM_SENSORS 8
+#define EMITTER_PIN 9
+
+QTRSensors qtra;
+NewPing sonar(trig, echo, maximum_distance);
+uint16_t sensorValues[NUM_SENSORS];
+int P = 0;
+int I = 0;
+int D = 0;
+int motorspeed = 0;
+int P_pasado = 0;
+int16_t position;
+int maria=0;
+
+
+////////////////////Dieu chinh PID/////////////////////////////////
+// Khai báo tốc độ ban đầu
+int TocDobandau = 85;
+
+
+//float KP = 0.19, KI = 0.001, KD = 0.4;
+//float KP = 0.19, KI = 0.007, KD = 0.41;
+//float KP = 0.15, KI = 0.007, KD = 0;
+float KP=0.07, KI=0.001, KD=0.50;
+
+int boton2 = 7;
+
+void setup() {
+  // Tùy loại cảm biến sử dụng thì khai báo theo loại cảm biến đó
+  qtra.setTypeAnalog();
+  //qtra.setSensorPins((const uint8_t[]){A7,A6,A5,A4,A3,A2,A1,A0}, NUM_SENSORS);
+  qtra.setSensorPins((const uint8_t[]) {A0, A1, A2, A3, A4, A5, A6, A7}, NUM_SENSORS);
+  qtra.setEmitterPin(EMITTER_PIN);
+  pinMode(led1, OUTPUT);
+  pinMode(bin1, OUTPUT);
+  pinMode(bin2, OUTPUT);
+  pinMode(pwmb, OUTPUT);
+  pinMode(ain1, OUTPUT);
+  pinMode(ain2, OUTPUT);
+  pinMode(pwma, OUTPUT);
+  pinMode(trig,OUTPUT);//chân trig sẽ phát tín hiệu
+  pinMode(echo,INPUT);//chân echo sẽ nhận tín hiệu
+  digitalWrite(led1, HIGH);
+  delay(200);
+  digitalWrite(led1, LOW);
+  delay(200);
+digitalWrite(led1, HIGH);
+Serial.begin(115200);
+  digitalWrite(led1, HIGH); //bật led 13 lên để cho user biết đang trong chế độ calibrate     /**< Rotates the Robot */
+//  for (int i = 0; i < 400; i++) // calibration trong 10s
+//  {
+//    DieuKhienMotor(90,-90);
+//    qtra.calibrate(); // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
+//    Serial.println(i);
+//  }
+  for (int i = 0; i < 400; i++){
+    if (i % 5 == 0) {
+      DieuKhienMotor(80, -80);
+    }
+    Serial.println(i);
+    qtra.calibrate();
+    delay(20);
+  }
+  DieuKhienMotor(0, 0);
+  digitalWrite(led1, LOW); //tắt led 13
+  
+
+  while (true) {
+    botones();
+    if (boton2 == 0) {
+      delay(20);
+      digitalWrite(led1, HIGH);
+      delay(100);
+      digitalWrite(led1, LOW);
+      delay(100);
+      break;
+    }
+  }
+
+}
+
+void loop() {
+   for (uint8_t i = 0; i < NUM_SENSORS ; i++)
+   {
+   Serial.print(sensorValues[i]);
+   Serial.print('\t');
+   }
+   Serial.println();
+
+  pid(TocDobandau, KP, KI, KD);
+  Magic(950);
+  Serial.println(position);
+}
+
+void pid(int TocDobandau, float Kp, float Ki, float Kd) {
+  position = qtra.readLineWhite(sensorValues);
+
+  // Serial.println(position);
+  // int error=3500 - position;
+  // P = error;
+  // I = I + error;
+  // D = error - lastError;
+  // lastError = error;
+  //
+  P = 3500 - position; // set point es 3500, asi obtenemos el error
+  I = I + P_pasado; //obteniendo I
+  D = (P - P_pasado); //obteniedo el D
+  int ITerm = I * KI;
+  if (ITerm >= 255) ITerm = 255;
+  if (ITerm <= -255) ITerm = -255;
+  motorspeed = (P * KP) + (D * KD) + (ITerm);
+
+  if (motorspeed > TocDobandau) motorspeed = TocDobandau; //limitamos la salida de pwm
+  if (motorspeed < -TocDobandau) motorspeed = -TocDobandau;
+
+  if (motorspeed < 0) {
+    int speedLeft = TocDobandau - motorspeed; //(+)
+    int speedRight = TocDobandau + motorspeed; //(-)
+    if (speedLeft >= 255) speedLeft = 255;
+    if (speedRight <= 0) speedRight = 0;
+    DieuKhienMotor(speedRight, speedLeft);
+  }
+  if (motorspeed > 0) {
+    int speedLeft = TocDobandau - motorspeed; //(-)
+    int speedRight = TocDobandau + motorspeed; //(+)
+
+    if (speedRight >= 255) speedRight = 255;
+    if (speedLeft <= 0) speedLeft = 0;
+    DieuKhienMotor(speedRight, speedLeft);
+  }
+
+  P_pasado = P;
+}
+
+void Magic(int ChiSo) {
+  if (position >= 6990)
+  {
+    Serial.println("Truong hop >= 6990 Cam bien:");
+    while (true) {
+      digitalWrite(led1, HIGH);
+        DieuKhienMotor(125, 125);
+      qtra.readLineWhite(sensorValues); //lectura en bruto de sensor
+      for (uint8_t i = 0; i < NUM_SENSORS ; i++)
+       {
+       Serial.print(sensorValues[i]);
+       Serial.print('\t');
+       }
+       Serial.println();
+      if (sensorValues[0] > ChiSo || sensorValues[1] > ChiSo || sensorValues[2] > ChiSo ||
+          sensorValues[3] > ChiSo || sensorValues[4] > ChiSo || sensorValues[5] > ChiSo ||
+          sensorValues[6] > ChiSo || sensorValues[7] > ChiSo) {
+        break;
+      }
+
+    }
+  }
+
+
+      if((position>3400 && position<3600) && (sensorValues[0] < 300 && sensorValues[1] < 300 && sensorValues[2] < 300 &&
+          sensorValues[3] > ChiSo && sensorValues[4] > ChiSo && sensorValues[5] > ChiSo &&
+          sensorValues[6] > ChiSo && sensorValues[7] > ChiSo) ||  (sensorValues[0] < 300 && sensorValues[1] < 300 && sensorValues[2] < 300 &&
+          sensorValues[3] < 400 && sensorValues[4] > ChiSo && sensorValues[5] > ChiSo &&
+          sensorValues[6] > ChiSo && sensorValues[7] > ChiSo)){
+    Serial.println("Truong hop >= 6990 Cam bien:");
+        DieuKhienMotor(-40, -40); 
+    DieuKhienMotor(0, 0);
+    while (true) {
+      digitalWrite(led1, HIGH);
+        DieuKhienMotor(100, -100);
+        
+        
+     //DieuKhienMotor(125, -125);
+      qtra.readLineWhite(sensorValues); //lectura en bruto de sensor
+      for (uint8_t i = 0; i < NUM_SENSORS ; i++)
+       {
+       Serial.print(sensorValues[i]);
+       Serial.print('\t');
+       }
+       Serial.println();
+      if (sensorValues[0] > ChiSo || sensorValues[1] > ChiSo || sensorValues[2] > ChiSo ||
+          sensorValues[3] > ChiSo) {
+            delay(10);
+        break;
+      }
+
+    }
+  }
+    if((position>3400 && position<3600) && (sensorValues[0] < 300 && sensorValues[1] < 100 && sensorValues[2] < 100 &&
+          sensorValues[3] < 100 && sensorValues[4] < 100 && sensorValues[5] < 100 &&
+          sensorValues[6] < 100 && sensorValues[7] < 100)){
+    Serial.println("Truong hop >= 6990 Cam bien:");
+        DieuKhienMotor(-40, -40); 
+    DieuKhienMotor(0, 0);
+    while (true) {
+      digitalWrite(led1, HIGH);
+      if(sensorValues[7] > 500){
+        DieuKhienMotor(140, -100);
+        }
+        else{
+           DieuKhienMotor(125, -125);
+        }
+        
+     //DieuKhienMotor(125, -125);
+      qtra.readLineWhite(sensorValues); //lectura en bruto de sensor
+      for (uint8_t i = 0; i < NUM_SENSORS ; i++)
+       {
+       Serial.print(sensorValues[i]);
+       Serial.print('\t');
+       }
+       Serial.println();
+      if (sensorValues[0] > ChiSo || sensorValues[1] > ChiSo || sensorValues[2] > ChiSo ||
+          sensorValues[3] > ChiSo || sensorValues[4] > ChiSo || sensorValues[5] > ChiSo ||
+          sensorValues[6] > ChiSo || sensorValues[7] > ChiSo) {
+        break;
+      }
+
+    }
+  }
+
+  if (position <= 1500 && position >=500) 
+  {
+    
+    while (true) {
+      digitalWrite(led1, HIGH);
+      DieuKhienMotor(150, -120);
+      qtra.readLineWhite(sensorValues);
+      for (uint8_t i = 0; i < NUM_SENSORS ; i++)
+       {
+       Serial.print(sensorValues[i]);
+       Serial.print('\t');
+       }
+       Serial.println();
+       
+      if ((position <= 1500 && position >=500) &&
+          (sensorValues[4] > ChiSo || sensorValues[5] > ChiSo
+          )) {
+      
+        break;
+      }
+    }
+  }
+  digitalWrite(led1, LOW);
+}
+
+void DieuKhienMotor(int motor_phai, int motor_trai) {
+  if (motor_phai >= 0) {
+    digitalWrite(bin1, LOW);
+    digitalWrite(bin2, HIGH);
+    analogWrite(pwmb, motor_phai);
+  } else {
+    digitalWrite(bin1, HIGH);
+    digitalWrite(bin2, LOW);
+    motor_phai = motor_phai * (-1);
+    analogWrite(pwmb, motor_phai);
+  }
+
+  if (motor_trai >= 0) {
+    digitalWrite(ain1, LOW);
+    digitalWrite(ain2, HIGH);
+    analogWrite(pwma, motor_trai);
+  } else {
+    digitalWrite(ain1, HIGH);
+    digitalWrite(ain2, LOW);
+    motor_trai = motor_trai * (-1);
+    analogWrite(pwma, motor_trai);
+  }
+}
+
+void botones() {
+  boton2 = digitalRead(btn1);
+}
+int readPing(){
+  int cm = sonar.ping_cm();
+  if (cm==0){
+    cm=250;
+  }
+  return cm;
+}
